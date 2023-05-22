da4b322f473f3fc9e611ef9b7b0128116aad4450
lefwagner <130977855+lefwagner@users.noreply.github.com>
2023-05-22 19:19:26 +0200

    Add files via upload

---
+++ closed-captions/sounds.js
@@ -0,0 +1,23 @@
+// Hier kan je tijden toevoegen in secondes
+// Op die mommenten komt er een class op de body.
+sounds = [
+	0,
+	9,
+	9.8,
+	12,
+	13,
+	15,
+	16.3,
+	23.4,
+	24.3,
+	25.8,
+	26.8,
+	30.7,
+	32,
+	33,
+	40,
+	50,
+	55,
+	75,
+	86
+];
